528db1e59cb1c166e59e1d81eb6ac53aaf85d0a4
Yiliang Chen <yiliangchen0113@gmail.com>
2021-02-11 17:27:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 class Dog 
   def name= (dog_name)
-    this_dogs_name = dog_name
+    @this_dogs_name = dog_name
   end
   
   def name 
-    this_dogs_name
+    @this_dogs_name
   end 
   
 end 
@@ -12,4 +12,4 @@ end
 lassie = Dog.new 
 lassie.name = "Lassie"
 
-lassie.name
+puts lassie.name
